c8ac559276b0fd9d929fc8e201b43d69e73a6207
bengabp <bengabdev4@gmail.com>
2025-01-15 22:40:41 +0000

    implement operator order in matchs_exp.cpp

---
+++ variables/maths_exp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main () {
+    // Addition
+    // int x = 5;
+    // int y = 30;
+    // int z = x + y;
+    // std::cout << z;
+
+    double x = 10;
+    int y = 5;
+    double z = (x + 10) / (3* y);
+
+    std::cout << z;
+
+    return 0;
+}
