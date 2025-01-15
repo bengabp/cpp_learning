a9c0a486b57a9fb44bd4eec940a2ccf01e02906c
bengabp <bengabdev4@gmail.com>
2025-01-15 22:26:41 +0000

    Add variables

---
+++ variables/first_learn.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main () {
+    int age = 22;
+    double height = 5.9;
+    int x = 5;
+    int y = 10;
+
+    
+
+    // // print the sum of x and y
+    // std::cout << x + y;
+    // // print the difference when y is subtracted from x
+    // std::cout << x - y;
+    // // print the product of x and y
+    // std::cout << x * y;
+    // print the quotient of x and y
+    // std::cout << x / y;
+
+    return 0;
+}
---
+++ variables/naming.cpp
@@ -0,0 +1,13 @@
+
+int main() {
+    double file_size = 0; // snake_case convention
+    double FileSize = 3.3; // Pascal case convention
+    double fileSize = 343.34; // camelCase convention
+    double dFileSize = 343.343; // Hungarian notation - the first letter is the type of the variable
+
+    /*
+    variables and constants - camel case
+    functions - 
+    classes - pascal case
+    */
+}
