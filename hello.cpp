05d8c9b722c0fa755fb6f6b6b7f256e20eb4f074
bengabp <bengabdev4@gmail.com>
2025-01-15 21:49:16 +0100

    Add files

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Statements should be terminated with a semi-colon
+    std::cout << "Hello world";
+    return 0; // Returning 0 means program ran successfully
+}
