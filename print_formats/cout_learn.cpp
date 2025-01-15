2fe365ba2915837c0c32b2fa93871600d95a050d
bengabp <bengabdev4@gmail.com>
2025-01-15 22:50:02 +0000

    Add print_formats

---
+++ print_formats/cout_learn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int age = 22;
+    double height = 5.9;
+
+    cout << "My age is " << age << endl
+        << "My height is " << height << endl;
+    
+    return 0;
+}
