de54be1fd16b8bf3481a8ed61df2da5065f8df62
bengabp <bengabdev4@gmail.com>
2025-01-17 01:09:34 +0100

    Complete decimal -> binary feature in number_system_converter

--- number_system_converter/main.cpp
+++ number_system_converter/main.cpp
@@ -14,16 +14,21 @@ std::string dequeToString(std::deque<short> deque){
 }
 
 std::deque<short> decimalToBinary(short decimalNumber) {
-    int reminder = 2;
-    int number = decimalNumber;
-    int quotient = 0;
+    int reminder, quotient, number = decimalNumber;
+    bool loop = true;
     std::deque<short> remindersQueue; // This allows pushing the reminders to the front
 
-    while (reminder > 1 ) {
+    while (loop) {
         quotient = number / 2;
         reminder = number - (quotient * 2); // Calculate reminder by subtracting the product of quotient and 2 from the current number
+        
+        std::cout << quotient << " | r " << reminder << std::endl;
         remindersQueue.push_front(reminder); // Append reminder to the front of the queue
         number = quotient; // Update the value of number so that it can be used in the next iteration
+        
+        if (number < 1){
+            loop = false;
+        }
     }
     return remindersQueue;
 }
@@ -33,8 +38,7 @@ int main () {
     std::cout << "Enter decimal number to convert to binary: ";
     std::cin >> decimalNumber;
     std::deque resultBinary = decimalToBinary(decimalNumber);
-    std::cout << "Decimal [" << decimalNumber << "] - Binary " 
+    std::cout << "Decimal [" << decimalNumber << "] to Binary " 
                         << "= " << dequeToString(resultBinary) << std::endl;
-    return 0;
-    
+    return 0;  
 }
