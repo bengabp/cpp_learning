01cadbf88b2abf275f35d4757cb97f58400a12d8
bengabp <bengabdev4@gmail.com>
2025-01-17 23:16:42 +0100

    Add simple_login_system

---
+++ simple_login_system/main.cpp
@@ -0,0 +1,18 @@
+/* 
+Building a login system in c++
+
+--> You can create new user account with username and password
+--> You can login with your username and password
+--> You can list all user profiles
+--> You can logout
+*/
+
+#include <iostream>
+#include <string>
+#include <deque>
+
+
+int main() {
+
+}
+
